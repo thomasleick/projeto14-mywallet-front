655650aeeb7f1158c0f3bae3b2d1d62f2e0c12cb
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-04-21 19:23:49 -0300

    { props }

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Transaction = (props) => {
-    const { data, description, type, value } = props.props
+const Transaction = ({ props }) => {
+    const { data, description, type, value } = props
     return (
         <ListItemContainer>
         <div>
