6e7304f48412281379d2c648a99538afdf8f3420
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-04-25 14:17:26 -0300

    reset before global style

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client"
 import App from './App';
 import { AuthProvider } from './context/AuthProvider'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import GlobalStyle from "./style/GlobalStyle"
 import ResetStyle from "./style/ResetStyle"
+import GlobalStyle from "./style/GlobalStyle"
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
